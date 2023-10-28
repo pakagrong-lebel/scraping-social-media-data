5180c8c8c2d4ba2d50824984502b8f552f002788
pakagrong-lebel <92211586+pakagrong-lebel@users.noreply.github.com>
2023-10-28 18:10:16 +1100

    Add files via upload

---
+++ scraping.py
@@ -0,0 +1,5 @@
+# Python script for scraping data from social media platforms
+import requests
+def scrape_social_media_data(url):
+    response = requests.get(url)
+    # Your code here to extract relevant data from the response
